85c6a9df83490819d72521c2cf00fb690fd34f5b
starman <th.sarigiorgis@gmail.com>
2019-06-14 01:19:35 +0300

    Add project files.

---
+++ BootFlixHome/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BootFlixHome.MvcApplication" Language="C#" %>
---
+++ BootFlixHome/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootFlixHome.Startup))]
+namespace BootFlixHome
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
